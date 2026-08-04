9c399d670d40516a6a4a038a9112bd79f3e49d65
Lucavinini <lucas.00000850855@unicap.br>
2026-08-04 12:27:55 -0300

    feat: adiciona arquivo explicando padrão camelCase

---
+++ variaveis/CaseCamel.go
@@ -0,0 +1,10 @@
+//Padrão para escrever o nome de variáveis
+// Cada Palavra, menos a primeira começa com letra maiúscula
+
+/*
+ * Ex.:
+ * 
+ * var minhaIdade int;
+ * 
+ * myVariavelName = "Lucas"
+ */
--- variaveis/sintaxe.go
+++ variaveis/sintaxe.go
@@ -6,7 +6,7 @@ import ("fmt") //Importação de pacotes a serem usados
 func main(){ //Função principal
 	fmt.Println("Oláaaa") //Declarações e expressões
 
-	//Declaração tradicional var + nome + tipo
+	/* Declaração tradicional var + nome + tipo
 	
 	var MinhaIdade int = 10
 	var MeuPeso float32 = 68.02
@@ -14,5 +14,20 @@ func main(){ //Função principal
 	var StatusCorteDeCabelo bool = false
 	
 	fmt.Println(MinhaIdade, MeuPeso, MeuNome, StatusCorteDeCabelo)
+	*/
+	
+	// Declaração inferida - tipo da variável conforme o valor
+	// variablename := value
+	// var = value
+
+	/*
+	x := 40
+	var student2 = "Jane"
+	fmt.Println(student2, x)
+	*/
+
+	var studentname string
+	studentname = "Lucas"
+	fmt.Println(studentname)
 	
 }
